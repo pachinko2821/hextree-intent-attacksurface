37dc8f68218c117ee7334ab46c6fa8f2a3a9a75a
Prajyot Chemburkar <prajyotchemburkar@gmail.com>
2026-07-31 20:59:33 +0530

    added flag 15

--- app/src/main/java/com/pachinko/intentattacksurface/utils/Flag15.kt
+++ app/src/main/java/com/pachinko/intentattacksurface/utils/Flag15.kt
@@ -1,9 +1,14 @@
 package com.pachinko.intentattacksurface.utils
 
 import android.content.Context
+import android.content.Intent
+import androidx.core.net.toUri
 
 object Flag15 {
     fun getFlag(context: Context) {
-        TODO("Implement Flag 15")
+        val intent = Intent().apply {
+            data = "intent://ad/#Intent;package=io.hextree.attacksurface;component=io.hextree.attacksurface/io.hextree.attacksurface.activities.Flag15Activity;action=io.hextree.action.GIVE_FLAG;end".toUri()
+        }
+        context.startActivity(intent)
     }
 }
