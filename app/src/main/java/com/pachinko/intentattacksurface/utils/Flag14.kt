567b47979c5bca46b3934d9ab959935683261f48
Prajyot Chemburkar <prajyotchemburkar@gmail.com>
2026-07-31 16:49:03 +0530

    added flag 14

--- app/src/main/java/com/pachinko/intentattacksurface/utils/Flag14.kt
+++ app/src/main/java/com/pachinko/intentattacksurface/utils/Flag14.kt
@@ -1,9 +1,18 @@
 package com.pachinko.intentattacksurface.utils
 
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.pm.ComponentInfo
 
 object Flag14 {
     fun getFlag(context: Context) {
-        TODO("Implement Flag 14")
+        val intent = Intent().apply {
+            component = ComponentName(
+                "io.hextree.attacksurface",
+                "io.hextree.attacksurface.activities.Flag14Activity"
+            )
+        }
+        context.startActivity(intent)
     }
 }
