ce32035fe6d1338548db40c8c52da782bad2c507
Prajyot Chemburkar <prajyotchemburkar@gmail.com>
2026-08-01 00:22:24 +0530

    added flags 24 and 25

--- app/src/main/java/com/pachinko/intentattacksurface/utils/Flag24.kt
+++ app/src/main/java/com/pachinko/intentattacksurface/utils/Flag24.kt
@@ -1,9 +1,19 @@
 package com.pachinko.intentattacksurface.utils
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 
 object Flag24 {
     fun getFlag(context: Context) {
-        TODO("Implement Flag 24")
+        val intent = Intent().apply {
+            setClassName(
+                "io.hextree.attacksurface",
+                "io.hextree.attacksurface.services.Flag24Service"
+            )
+            action = "io.hextree.services.START_FLAG24_SERVICE"
+        }
+        Log.d("Flag24", "Starting service")
+        context.startService(intent)
     }
 }
--- app/src/main/java/com/pachinko/intentattacksurface/utils/Flag25.kt
+++ app/src/main/java/com/pachinko/intentattacksurface/utils/Flag25.kt
@@ -1,9 +1,26 @@
 package com.pachinko.intentattacksurface.utils
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.milliseconds
 
 object Flag25 {
     fun getFlag(context: Context) {
-        TODO("Implement Flag 25")
+        for(lock in 1..3) {
+            val intent = Intent().apply {
+                setClassName(
+                    "io.hextree.attacksurface",
+                    "io.hextree.attacksurface.services.Flag25Service"
+                )
+                action = "io.hextree.services.UNLOCK${lock}"
+            }
+            runBlocking {
+                delay(3000.milliseconds)
+                context.startService(intent)
+            }
+        }
     }
 }
